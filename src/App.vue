290cdeac7dcfbfc8885e5daf5612421eea7700af
Arisha <arisha.alee@gmail.com>
2022-04-16 00:30:18 +0500

    Initialized project

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <!-- // you can build your nav here -->
+  <router-view />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
