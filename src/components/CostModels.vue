b366d4f5cd51ed73547ba747c81d49569416984d
Arisha <arisha.alee@gmail.com>
2022-04-18 15:37:52 +0500

    Added cost models

--- src/components/CostModels.vue
+++ src/components/CostModels.vue
@@ -1,15 +1,25 @@
 <template>
      <div class="mr-72 flex flex-col">
       <div class="text-md font-bold">Cost Models</div>
-      <div class="bg-white p-4 rounded-md">
+      <div class="bg-white p-4 h-96 rounded-md sm:rounded-lg scrollbar-thin scrollbar-thumb-black scrollbar-track-gray-300 overflow-y-scroll ">
           <div class="block">
-              <div class="px-2 py-1">
+              <div class="p-2" v-for="model in costModels" :key='model'>
                 <label class="flex items-center">
                   <input type="checkbox"/>
-                  <span class="ml-2">Assign to Solution Designer</span>
+                  <span class="ml-2">{{ model }}</span>
                 </label>
               </div>
             </div>
       </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return{
+    costModels: ['Software Development/Customization', 'Application Install/ de-Install', 'Application Configuration', 'Application Licenses', 'Application Configuration', 'Application Support & Maintenance', 'Applications Integration', 'Application Migration', 'Application Operation', 'Application Hosting', 'Hardware Implementation', 'HW Support & Maintenance', 'Application Hosting', 'Hardware Implementation', 'HW Support & Maintenance']
+    }
+  }
+}
+</script>
