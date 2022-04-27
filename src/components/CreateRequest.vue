87987f803a46e92b6920a4b35e7c648a6fe6e9ca
Arisha <arisha.alee@gmail.com>
2022-04-27 13:16:37 +0500

    create request api

--- src/components/CreateRequest.vue
+++ src/components/CreateRequest.vue
@@ -1,5 +1,5 @@
 <template>
-  <form enctype="multipart/form-data">
+  <form @submit.prevent="OnUpload" enctype="multipart/form-data">
   <div
     class="w-full flex items-center flex-col md:flex-row lg:flex-row justify-center"
   >
@@ -37,12 +37,11 @@
         </p>
       </div>
       <div class="flex flex-col items-center">
-        <label class="text-white bg-black p-2 w-56 rounded-md mt-6">
-          <input type="file" ref="file" @change="FileUpload()" />
+        <label class="text-white bg-black p-2 rounded-md mt-6">
+          <input type="file"  @change="onFileSelected"/>
         </label>
-        <button
-          @click="CreateRequest"
-          class="mb-4 text-white bg-black p-2 w-64 rounded-md mt-12"
+        <button 
+          class="mb-4 text-white bg-black py-2 px-4 rounded-md mt-12"
         >
           Send Requirements
         </button>
@@ -51,3 +50,41 @@
   </div>
   </form>
 </template>
+
+<script>
+import axios from 'axios'
+export default {
+  data() {
+    return{
+      name: '',
+      description: '',
+      selectedFile: null
+    }
+  },
+  methods: {
+    onFileSelected(event) {
+      this.selectedFile = event.target.files[0]
+    },
+    async OnUpload() {
+      const fd = new FormData();
+      fd.append('name', this.name)
+      fd.append('description', this.description)
+      fd.append('attachment',this.selectedFile, this.selectedFile.name)
+
+      let token = localStorage.getItem('token')
+      return await axios.post('http://127.0.0.1:8000/request/create/', fd, {headers: {
+        "Authorization" : "Token " + token
+      }})
+      .then(res => {
+        console.log(res)
+        this.name = '',
+        this.description = '',
+        this.selectedFile = ''
+      })
+      .catch(error => {
+        console.log(error);
+    })
+    }
+  },
+}
+</script>
