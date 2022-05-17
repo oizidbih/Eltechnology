b8766cf22e2e4915f353afbf7cb8e3cdced2e9f2
Arisha <arisha.alee@gmail.com>
2022-05-17 17:33:49 +0500

    removed vertical line

--- src/components/DMsidebar.vue
+++ src/components/DMsidebar.vue
@@ -2,7 +2,7 @@
     <div class="mt-6 md:mt-0 fixed bg-black w-screen bottom-0 lg:h-screen lg:w-16 lg:fixed md:fixed lg:top-16">
        <div class="flex justify-around -mt-2 mb-1 items-center lg:flex-col">
        <router-link v-for="page in pages" :key="page" :to=page.path >
-       <div :class=" [current == page.name ? 'border-b-2 border-white lg:border-l-2 text-white mt-6 text-xs': 'text-gray-300 mt-5 text-sm']">
+       <div :class=" [current == page.name ? 'border-b-2 border-white text-white mt-6 text-xs': 'text-gray-300 mt-5 text-sm']">
            <div class="flex items-center flex-col">
             <a href="#"><img :src="page.icon" class="h-6 w-6" alt=""></a>
             <p class="text-sm">{{ page.name }}</p>
--- src/components/ManagerSidebar.vue
+++ src/components/ManagerSidebar.vue
@@ -2,7 +2,7 @@
   <div class="fixed bg-black w-screen bottom-0 lg:h-screen lg:w-16 lg:fixed md:fixed lg:top-16">
        <div class="flex justify-around -mt-2 mb-1 items-center lg:flex-col">
         <router-link v-for="page in pages" :key="page" :to=page.path >
-       <div :class=" [current == page.name ? 'sm:border-b-2 border-white md:border-l-2 text-white mt-6 text-xs': 'text-gray-300 mt-5 text-sm']">
+       <div :class=" [current == page.name ? 'sm:border-b-2 border-white text-white mt-6 text-xs': 'text-gray-300 mt-5 text-sm']">
            <div class="flex items-center flex-col">
             <a href="#"><img :src="page.icon" class="h-6 w-6" alt=""></a>
             <p class="text-sm">{{ page.name }}</p>
