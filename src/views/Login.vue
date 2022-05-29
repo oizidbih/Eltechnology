7d4068ae088bc86a9824c3bea85b3da2e33f854a
Arisha <arisha.alee@gmail.com>
2022-05-29 22:47:20 +0500

    changed width of image

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,7 +3,7 @@
         <Mainbar />
     <div class="flex justify-center ">
     <div class="w-full bg-white m-2 lg:w-7/12 lg:h-144 lg:my-12 rounded-md flex flex-row justify-between">
-        <div class="w-24 ml-1 h-144 sm:w-64 sm:ml-4 lg:w-80 lg:ml-4 ">
+        <div class="w-20 ml-1 h-144 sm:w-64 sm:ml-4 lg:w-80 lg:ml-4 ">
             <img src='../assets/Login-pattern.png' class="w-full h-full" alt="Pattern">
         </div>
         <LoginForm />
