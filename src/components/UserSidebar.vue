d43edd4caa6719fc649ca3c0cb88800adb3ee959
Arisha <arisha.alee@gmail.com>
2022-05-29 23:20:09 +0500

    sidebar changes

--- src/components/UserSidebar.vue
+++ src/components/UserSidebar.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="mt-6 md:mt-0 sticky bg-black w-screen bottom-0 lg:h-screen lg:w-16 lg:fixed md:fixed lg:top-16">
-       <div class="flex justify-around -mt-2 mb-1 items-center lg:flex-col">
+    <div class="mt-6 md:mt-0 sticky bg-black w-screen bottom-0 lg:h-screen lg:w-16 lg:fixed md:fixed lg:top-16 overflow-x-hidden">
+       <div class="flex justify-evenly sm:justify-around -mt-2 mb-1 items-center lg:flex-col">
        <router-link v-for="page in pages" :key="page" :to=page.path >
        <div :class=" [current == page.name ? 'border-b-2 border-white text-white mt-6 text-xs': 'text-gray-300 mt-5 text-sm']">
-           <div class="flex items-center flex-col">
-            <a href="#"><img :src="page.icon" class="h-6 w-6" alt=""></a>
-            <p class="text-sm">{{ page.name }}</p>
+           <div class="flex items-center flex-col ">
+            <a href="#"><img :src="page.icon" class="h-5 w-5 sm:h-6 sm:w-6" alt=""></a>
+            <p class="text-xs sm:text-sm">{{ page.name }}</p>
            </div>
        </div>
         </router-link>
