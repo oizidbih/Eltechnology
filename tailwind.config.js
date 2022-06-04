f1911bf34838bd4c4664da2faf64883523815056
syedbilal28 <syed.bilal.sba@gmail.com>
2022-06-04 23:32:17 +0500

    tailwind config added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,27 @@ module.exports = {
     "./index.html",
     "./src/**/*{vue,js,ts,jsx,tsx}"  
   ],
+  darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        '112' : '28rem',
+        '128': '32rem',
+        '144' : '36rem'
+      },
+      width: {
+        '112' : '28rem',
+        '128': '32rem',
+        '144' : '36rem'
+      },
+      colors: {
+        
+        Light: '#FFFFFF'
+      }
+    },
   },
-  plugins: [],
+  plugins: [require('tailwind-scrollbar')],
+  variants:{
+    extend: {},
+  }
 }
